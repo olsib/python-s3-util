fe9154f356086e8a3c1f5df0d8bef5bd2196afb0
olsib <olsi.birbo@gmail.com>
2016-10-30 14:20:09 +0000

    Refactored some code in utility

--- awss3utility/s3util.py
+++ awss3utility/s3util.py
@@ -2,7 +2,6 @@ import os
 import collections
 import datetime
 import mimetypes
-
 import pytz
 import boto
 from boto.ec2.cloudwatch import CloudWatchConnection
@@ -218,34 +217,6 @@ class S3Utility(object):
 
     def set_s3_bucket(self, bucket_name):
         self.s3_bucket = bucket_name
-
-    def make_s3_url(self, s3_key, extension='.png', expiration_seconds_from_now=None):
-        if not expiration_seconds_from_now:
-            expiration_seconds_from_now = datetime.timedelta(days=365).total_seconds()
-
-        if isinstance(s3_key, Key):
-            key = s3_key
-        else:
-            key = Key(bucket=self.get_s3_bucket(), name=s3_key)
-
-        return key.generate_url(
-            expires_in=expiration_seconds_from_now,
-            response_headers={'response-content-type': mimetypes.types_map[extension]}
-        )
-
-    def list_s3_files(self, folder):
-        """Returns a dict of filenames -> urls."""
-        bucket = self.get_s3_bucket()
-        files = collections.OrderedDict()
-        iterator = bucket.list(prefix=folder)
-        for key in iterator:
-            file_extension = os.path.splitext(key.name)[1]
-            url = self.make_s3_url(key, extension=file_extension)
-
-            # add the url to the dict by its filename
-            files[os.path.basename(key.name)] = url
-
-        return files
 
     def put_metric(self, name, value, statistics=None, timestamp=None, dimensions=None):
         """
